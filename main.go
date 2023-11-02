052762a4d025e2cf439792ae3312647afa984329
Dennisovan92 <denis.majorsky92@gmail.com>
2023-11-02 19:03:45 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/coredns/coredns/core/dnsserver"
-	_ "github.com/coredns/coredns/core/plugin"
 	"github.com/coredns/coredns/coremain"
 
 	_ "github.com/coredns/rrl/plugins/rrl"
